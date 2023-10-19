43a7197047ec01aa8fa2ba930645c1534569595c
= <=>
2023-10-19 10:49:47 +0000

    setting up build for data preprocessing

--- packages/reconstruction-image/etl_pipeline/run_colmap.sh
+++ packages/reconstruction-image/etl_pipeline/run_colmap.sh
@@ -4,9 +4,16 @@ DATASET_PATH_ETH="./dataset/courtyard/"
 
 colmap feature_extractor \
     --database_path=$DATASET_PATH_ETH/database.db \
-    --image_path=${1}/images/dslr_images_undistorted \
+    --image_path=$DATASET_PATH_ETH/images/dslr_images_undistorted \
     --ImageReader.camera_model=SIMPLE_RADIAL \
     --ImageReader.single_camera=true \
     --SiftExtraction.use_gpu=true \
     --SiftExtraction.num_threads=32
 
+echo "now extracting the sparse representation of the point cloud by matching the images"
+
+colmap sequential_matcher \
+    --database_path=$DATASET_PATH_ETH/database.db \
+    --SiftMatching.use_gpu=true
+
+echo "and then "
