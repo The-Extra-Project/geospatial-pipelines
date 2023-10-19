1d7df6d4386505ced7452be6b4fec582456913a4
Dhruv <malikdhruv1994@gmail.com>
2023-10-19 11:29:29 +0200

    - remove scripts from another packages -  init dockerfile for preprocessing scripts and colmap file

--- packages/reconstruction-image/etl_pipeline/run_colmap.sh
+++ packages/reconstruction-image/etl_pipeline/run_colmap.sh
@@ -1,40 +1,12 @@
 #!/bin/sh
 
+DATASET_PATH_ETH="./dataset/courtyard/"
 
 colmap feature_extractor \
-    --database_path=${1}/database.db \
-    --image_path=${1}/images_raw \
+    --database_path=$DATASET_PATH_ETH/database.db \
+    --image_path=${1}/images/dslr_images_undistorted \
     --ImageReader.camera_model=SIMPLE_RADIAL \
     --ImageReader.single_camera=true \
     --SiftExtraction.use_gpu=true \
     --SiftExtraction.num_threads=32
 
-colmap sequential_matcher \
-    --database_path=${1}/database.db \
-    --SiftMatching.use_gpu=true
-
-mkdir -p ${1}/sparse
-colmap mapper \
-    --database_path=${1}/database.db \
-    --image_path=${1}/images_raw \
-    --output_path=${1}/sparse
-
-cp ${1}/sparse/0/*.bin ${1}/sparse/
-for path in ${1}/sparse/*/; do
-    m=$(basename ${path})
-    if [ ${m} != "0" ]; then
-        colmap model_merger \
-            --input_path1=${1}/sparse \
-            --input_path2=${1}/sparse/${m} \
-            --output_path=${1}/sparse
-        colmap bundle_adjuster \
-            --input_path=${1}/sparse \
-            --output_path=${1}/sparse
-    fi
-done
-
-colmap image_undistorter \
-    --image_path=${1}/images_raw \
-    --input_path=${1}/sparse \
-    --output_path=${1} \
-    --output_type=COLMAP
